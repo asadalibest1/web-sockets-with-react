bb8ca46c7e39cbd850cb18f0f53d7b842cfa8d3f
Asad Ali <asadalibest1@gmail.com>
2022-11-29 18:13:55 +0500

    finished

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import "./App.css"
+
+var client = new W3CWebSocket('ws://localhost:8000/', 'echo-protocol');
+const App = () => {
+
+    const [getData, setgetData] = useState('')
+    const [Error, setError] = useState("");
+    const sendData = (e) => {
+        if (client.readyState === client.OPEN) {
+            setgetData(e.target.value)
+            client.send(e.target.value);
+            // setTimeout(sendNumber, 1000);
+        }
+    }
+
+    useEffect(() => {
+
+        // function sendNumber() {
+        //     if (client.readyState === client.OPEN) {
+        //         var number = Math.round(Math.random() * 0xFFFFFF);
+        //         client.send(number.toString());
+        //         setTimeout(sendNumber, 1000);
+        //     }
+        // }
+
+
+        client.onerror = function () {
+            console.log('Connection Error');
+            setError("Server conncetion failed!");
+        }
+
+        client.onopen = function () {
+            console.log('WebSocket Client Connected');
+            setError("WebSocket Client Connected!");
+
+        };
+
+        client.onclose = function () {
+            console.log('echo-protocol Client Closed');
+        };
+
+        client.onmessage = function (e) {
+            if (typeof e.data === 'string') {
+                console.log(e.data);
+                setgetData(e.data);
+            }
+        };
+
+    }, [])
+
+    return (
+        <>
+            <h1>Web Socket</h1>
+            <textarea value={getData} onChange={sendData} rows={5} cols={50} />
+            {Error && <div className='alert'>{Error}</div>}
+            {/* <h2>Connections:</h2>
+            <ul>
+
+            </ul> */}
+        </>)
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,65 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+const { getUniqueID } = require('./assests/UniqueId.js');
+
+var server = http.createServer(function (request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+});
+server.listen(8000, function () {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+var clients = {};
+
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  var connection = request.accept('echo-protocol', request.origin);
+  clients[userID] = connection;
+
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  connection.on('message', function (message) {
+    if (message.type === 'utf8') {
+
+      for (key in clients) {
+        if (clients[key] != clients[userID]) {
+          clients[key].sendUTF(message.utf8Data);
+          console.log('sent Message to: ', clients[key]);
+        }
+      }
+    }
+    else if (message.type === 'binary') {
+      for (key in clients) {
+        console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        connection.sendBytes(message.binaryData);
+      }
+    }
+  });
+  connection.on('close', function (reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
