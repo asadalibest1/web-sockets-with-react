56525c455cf860cb81770123c2bb4f54ba0f79fb
Asad Ali <asadalibest1@gmail.com>
2022-12-01 18:28:27 +0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -28,14 +28,6 @@ const App = () => {
 
     useEffect(() => {
 
-        // function sendNumber() {
-        //     if (client.readyState === client.OPEN) {
-        //         var number = Math.round(Math.random() * 0xFFFFFF);
-        //         client.send(number.toString());
-        //         setTimeout(sendNumber, 1000);
-        //     }
-        // }
-
 
         client.onerror = function () {
             console.log('Connection Error');
